df02b543914e127217bd3fcd5390b6cc72cfd1e4
Dzmitry Danilau <dzmitry_danilau1@epam.com>
2021-12-16 10:38:25 +0300

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED*** This import should be commented out in production mode because it will have a negative impact
+***REMOVED*** on performance if an error is thrown.
+***REMOVED***/
+***REMOVED***
